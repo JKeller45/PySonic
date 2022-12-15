796a6161cf17233e495875d004131c4ee669aace
jkeller45 <jkeller45@gatech.edu>
2022-12-15 13:57:57 -0600

    background and sinoidal damping

--- Audio_Processing.py
+++ Audio_Processing.py
@@ -1,78 +1,27 @@
 import scipy.io.wavfile as wavfile
-import scipy
-import scipy.fftpack as fftp
 import numpy as np
-from matplotlib import pyplot as plt
 import cv2
-from math import log10
 from multiprocessing import Pool
 import tomllib as tl
 from moviepy.editor import VideoFileClip, AudioFileClip
+import Functions as F
+from copy import deepcopy
 
-FILE = "C:/Users/james/Desktop/All-The-Kids-Are_depressed_remix.wav"
-color = [255, 255, 255]
-
-def plot_fft(freqs_side, FFT_side):
-    plt.subplot(311)
-    p3 = plt.plot(freqs_side, abs(FFT_side), "b")
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Count single-sided')
-    plt.xscale("log")
-    plt.xlim((0,25000))
-    plt.show()
-
-def calc_fft(args):
-    start, stop, step, signal = args
-    t = np.arange(start, stop, step)
-    FFT = abs(fftp.fft(signal))
-    FFT_side = FFT[range(len(FFT)//2)]
-    freqs = fftp.fftfreq(signal.size, t[1]-t[0])
-    freqs_side = freqs[range(len(FFT)//2)]
-    return freqs_side, FFT_side
-
-def draw_rect(output_image, xcoord, ycoord, width, height, color):
-    for y in range(int(height)):
-        for x in range(width):
-            output_image[ycoord - y][xcoord + x] = color
-
-def draw_bars(args):
-    num_bars, heights, color, width, separation = args
-    output_image = np.zeros((1080,1920,3), np.uint8)
-    xcoord = 0
-    for i in range(num_bars):
-        draw_rect(output_image, xcoord, 1079, width, heights[i] + 1, color)
-        xcoord += (width + separation)
-    return output_image
-
-def bins(freq, amp, heights, num_bars):
-    for c,v in enumerate(freq):
-        if v == 0:
-            continue
-        freq[c] = log10(v)
-    group_size = freq[-1] // num_bars
-    bins = np.linspace(log10(20),log10(25000),num_bars)
-    for c,v in enumerate(bins):
-        if c == 0:
-            continue
-        for i,f in enumerate(freq):
-            if f <= bins[c - 1]:
-                continue
-            if f > v:
-                break
-            heights[c] += amp[i]
-    heights = heights / 1_000_000_000
-    if max(heights) > 300:
-        heights = heights / (max(heights) / 300)
-    return heights
 
 if __name__ == "__main__":
     with open("config.toml", "rb") as f:
         config = tl.load(f)
 
+    FILE = config["settings"]["FILE"]
+    color = config["settings"]["color"]
+    background = config["settings"]["background"]
     width = config["settings"]["width"]
     connected = config["settings"]["connected_bars"]
     separation = config["settings"]["separation"]
     frame_rate = config["settings"]["frame_rate"]
+
+    background = cv2.imread(background)
+    background = cv2.resize(background, (1920,1080), interpolation=cv2.INTER_AREA)
 
     if connected:
         separation = 0
@@ -101,7 +50,7 @@ if __name__ == "__main__":
         num_bars = 1919
     ffts = []
 
-    length_in_frames = 3000
+    length_in_frames = 1500
     length_in_seconds = 1/frame_rate * length_in_frames
 
     args = []
@@ -110,13 +59,13 @@ if __name__ == "__main__":
         curr_step += num_frames
         prev_step += num_frames
     with Pool(processes=10) as pool:
-        ffts = pool.map(calc_fft, args)
+        ffts = pool.map(F.calc_fft, args)
 
     args = []
     for n in ffts:
-        args.append((num_bars, bins(n[0], n[1], np.ones(num_bars), num_bars), color, width, separation))
+        args.append((deepcopy(background), num_bars, F.bins(n[0], n[1], np.ones(num_bars), num_bars), color, width, separation))
     with Pool(processes=10) as pool:
-        output = pool.map(draw_bars, args)
+        output = pool.map(F.draw_bars, args)
 
     result = cv2.VideoWriter(f'{FILE}.mp4', cv2.VideoWriter_fourcc(*'mp4v'), frame_rate, (1920,1080))
     for f in output:
---
+++ Functions.py
@@ -0,0 +1,62 @@
+import scipy.fftpack as fftp
+import numpy as np
+from matplotlib import pyplot as plt
+from math import log10, sin, radians
+
+def plot_fft(freqs_side, FFT_side):
+    plt.subplot(311)
+    p3 = plt.plot(freqs_side, abs(FFT_side), "b")
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Count single-sided')
+    plt.xscale("log")
+    plt.xlim((0,25000))
+    plt.show()
+
+def calc_fft(args):
+    start, stop, step, signal = args
+    t = np.arange(start, stop, step)
+    FFT = abs(fftp.fft(signal))
+    FFT_side = FFT[range(len(FFT)//2)]
+    freqs = fftp.fftfreq(signal.size, t[1]-t[0])
+    freqs_side = freqs[range(len(FFT)//2)]
+    return freqs_side, FFT_side
+
+def draw_rect(output_image, xcoord, ycoord, width, height, color):
+    for y in range(int(height)):
+        for x in range(width):
+            output_image[ycoord - y][xcoord + x] = color
+
+def draw_bars(args):
+    backgroud, num_bars, heights, color, width, separation = args
+    xcoord = 0
+    for i in range(num_bars):
+        draw_rect(backgroud, xcoord, 1079, width, heights[i] + 1, color)
+        xcoord += (width + separation)
+    return backgroud
+
+def bins(freq, amp, heights, num_bars):
+    for c,v in enumerate(freq):
+        if v == 0:
+            continue
+        freq[c] = log10(v)
+    bins = np.linspace(log10(20),log10(25000),num_bars)
+    pointer = 1
+    for c,v in enumerate(freq):
+        if bins[pointer - 1] < v <= bins[pointer]:
+            add_height(heights, pointer, amp[c], 90, "middle")
+        else:
+            pointer += 1
+            add_height(heights, pointer, amp[c], 90, "middle")
+    heights = heights / 1_000_000_000
+    if max(heights) > 300:
+        heights = heights / (max(heights) / 300)
+    return heights
+
+def add_height(heights, group, amp, angle, side):
+    if angle <= 0 or group < 0 or group >= len(heights):
+        return
+    heights[group] += amp * sin(radians(angle))
+    if side == "left" or side == "middle":
+        add_height(heights, group - 1, amp, angle - 15, "left")
+    if side == "right" or side == "middle":
+        add_height(heights, group + 1, amp, angle - 15, "right")
