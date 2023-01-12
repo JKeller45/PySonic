d9c1c36eb12404688d2962b15e08543174009501
jkeller45 <jkeller45@gatech.edu>
2023-01-12 12:51:44 -0500

    Basic documentation for Functions.py

--- Functions.py
+++ Functions.py
@@ -6,6 +6,14 @@ import cv2
 from PIL import Image as im
 
 def plot_fft(freqs_side, FFT_side):
+    """
+    Plots the FFT with matplotlib
+
+    Parameters
+    -------
+    freqs_side (np.ndarray): the descrete frequency steps
+    FFT_side (np.ndarray): the amplitudes at each frequency step
+    """
     plt.subplot(311)
     p3 = plt.plot(freqs_side, abs(FFT_side), "b")
     plt.xlabel('Frequency (Hz)')
@@ -15,6 +23,18 @@ def plot_fft(freqs_side, FFT_side):
     plt.show()
 
 def alpha_composite(foreground, background):
+    """
+    Implementation of alpha composition. Used for overlaying RGBA images for partial/fully transparent overlays.
+
+    Parameters
+    ----------
+    foreground (np.ndarray): the image in the foreground
+    background (np.ndarray): the image being being composited on top of (the background)
+
+    Returns
+    -------
+    background (np.ndarray): the fully composited image
+    """
     alpha_background = background[:,:,3] / 255.0
     alpha_foreground = foreground[:,:,3] / 255.0
     for color in range(0, 3):
@@ -24,6 +44,21 @@ def alpha_composite(foreground, background):
     return background
 
 def calc_fft(args):
+    """
+    Calculates FFT for a given range. This method is designed to be used in a multithreaded way.
+
+    Parameters
+    ----------
+    start (int): the starting index to calculate from
+    stop (int): the ending index to calculate
+    step (int): the step size between samples
+    signal (np.ndarray): the audio signal to be processed
+
+    Returns
+    -------
+    freqs_side (np.ndarray): the descrete frequency steps
+    FFT_side (np.ndarray): the amplitudes at each frequency step
+    """
     start, stop, step, signal = args
     t = np.arange(start, stop, step)
     FFT = abs(fftp.fft(signal))
@@ -33,6 +68,21 @@ def calc_fft(args):
     return freqs_side, FFT_side
 
 def draw_rect(output_image, xcoord, ycoord, config, height):
+    """
+    Draws the bar of a given size for a given frame.
+
+    Parameters
+    ----------
+    output_image (np.ndarray): the background image being drawn over
+    xcoord (int): the x-coord being drawn at
+    ycoord (int): the y-coordinate being drawn at
+    config (dict): the config dictionary that contains the render settings
+    height (int): the height/length of each rectangle being drawn
+
+    Returns
+    -------
+    output_image (np.ndarray): the image with the rectangle drawn over it
+    """
     for y in range(int(height)):
         for x in range(config["width"]):
             if config["horizontal_bars"]:
@@ -48,6 +98,20 @@ def draw_rect(output_image, xcoord, ycoord, config, height):
     return output_image
 
 def draw_bars(args):
+    """
+    Draws the bars for a given frame. This method is designed to be used in a multithreaded way.
+
+    Parameters
+    ----------
+    background (np.ndarray): the background image being drawn over. Should be a deepcopy.
+    num_bars (int): the number of bars being drawn on the frame
+    heights (list): heights for each bar being drawn
+    config (dict): the config dictionary that contains the render settings
+
+    Returns
+    -------
+    background (np.ndarray): the final frame with all bars drawn over its
+    """
     background, num_bars, heights, config = args
     #transparent = np.zeros((len(backgroud), len(backgroud[0]), 4))
     offset = 0
@@ -78,6 +142,21 @@ def draw_bars(args):
     return background
 
 def bins(freq, amp, heights, num_bars, config):
+    """
+    Computes the heights of each bar from the FFT
+
+    Parameters
+    ----------
+    freq (np.ndarray): the frequency bins for the FFT
+    amp (np.ndarray): the total amplitudes at each frequency bin
+    heights (list): the list where heights will be added
+    num_bars (int): the number of bars in each frame
+    config (dict): the config dictionary that contains the render settings
+
+    Returns
+    -------
+    heights (list): the final heights of each bar in the frame
+    """
     for c,v in enumerate(freq):
         if v == 0:
             continue
@@ -100,6 +179,18 @@ def bins(freq, amp, heights, num_bars, config):
     return heights
 
 def add_height(heights, group, amp, angle, side, width):
+    """
+    Uses a sinoidal decay to add height to adjacent bars to give a more natural, non blocky, look to each frame.
+
+    Parameters
+    ----------
+    heights (list): the heights of each bar
+    group (int): the index of heights that is being added to
+    amp (int): the amplitude of the height being decayed
+    angle (int): the angle (in degrees) at the current point in the decay (range: 90 to 0)
+    side (str): whether its on the right or left side of the initial point. Used for recursion.
+    width (int): the width of each bar. Used for scaling the decay.
+    """
     if angle <= 0 or group < 0 or group >= len(heights):
         return
     heights[group] += amp * sin(radians(angle))
