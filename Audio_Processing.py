a1eea5a504c9fce9614dad2181301110857d1718
jkeller45 <jkeller45@gatech.edu>
2023-10-10 22:04:12 -0400

    appveyor improvements first attempt at automated testing mac-ffmpeg

--- Audio_Processing.py
+++ Audio_Processing.py
@@ -13,6 +13,7 @@ from itertools import cycle
 import numpy.typing as npt
 import subprocess
 import warnings
+from sys import platform
     
 def pick_react(args) -> npt.ArrayLike:
     background, num_bars, heights, avg_heights, settings = args
@@ -62,7 +63,10 @@ def render(config: dict, progress, main):
     # logging.log(logging.INFO, "Loading Audio...")
     CREATE_NO_WINDOW = 0x08000000
     if settings.audio_file[-4:] != ".wav":
-        convert_args = [F.find_by_relative_path(r"assets\ffmpeg\bin\ffmpeg.exe"),"-y", "-i", settings.audio_file, "-acodec", "pcm_s32le", "-ar", "44100", f"{settings.audio_file}.wav", ]
+        if platform == "win32":
+            convert_args = [F.find_by_relative_path(r"assets\ffmpeg\bin\ffmpeg.exe"),"-y", "-i", settings.audio_file, "-acodec", "pcm_s32le", "-ar", "44100", f"{settings.audio_file}.wav", ]
+        elif platform == "darwin":
+            convert_args = [F.find_by_relative_path(r"assets/ffmpeg/bin/ffmpeg"),"-y", "-i", settings.audio_file, "-acodec", "pcm_s32le", "-ar", "44100", f"{settings.audio_file}.wav", ]
         try:
             if subprocess.run(convert_args, creationflags=CREATE_NO_WINDOW).returncode == 0:
                 settings.audio_file = f"{settings.audio_file}.wav"
@@ -208,7 +212,11 @@ def render(config: dict, progress, main):
 
     # logging.log(logging.INFO, "Combining Audio...")
     print(f'{settings.output}{file_name}.mp4', settings.audio_file, f"{settings.output}{file_name}_Audio.mp4")
-    combine_cmds = [F.find_by_relative_path(r"assets\ffmpeg\bin\ffmpeg.exe"),"-y", "-i", f'{settings.output}{file_name}.mp4', '-i', settings.audio_file, '-map', '0', '-map', '1:a', '-c:v', 'copy', '-shortest', f"{settings.output}{file_name}_Audio.mp4"]
+    if platform == "win32":
+        combine_cmds = [F.find_by_relative_path(r"assets\ffmpeg\bin\ffmpeg.exe"),"-y", "-i", f'{settings.output}{file_name}.mp4', '-i', settings.audio_file, '-map', '0', '-map', '1:a', '-c:v', 'copy', '-shortest', f"{settings.output}{file_name}_Audio.mp4"]
+    elif platform == "darwin":
+        combine_cmds = [F.find_by_relative_path(r"assets/ffmpeg\bin/ffmpeg"),"-y", "-i", f'{settings.output}{file_name}.mp4', '-i', settings.audio_file, '-map', '0', '-map', '1:a', '-c:v', 'copy', '-shortest', f"{settings.output}{file_name}_Audio.mp4"]
+
     try:
         if subprocess.run(combine_cmds, creationflags=CREATE_NO_WINDOW).returncode == 0:
             os.remove(f'{settings.output}{file_name}.mp4')
---
+++ pysonic-testing.py
@@ -0,0 +1,26 @@
+import tomllib as tl
+from Audio_Processing import render
+from Classes import Progress_Spoof, Main_Spoof
+import cv2
+
+if __name__ == "__main__":
+    with open("testing/testing-config.toml", "rb") as f:
+        config = tl.load(f)
+        config = config["settings"]
+
+    render(config, Progress_Spoof(), Main_Spoof())
+
+    print("Done rendering, now validating...")
+    video = cv2.VideoCapture("testing/Avicii - Levels.webm.wav_Audio.mp4")
+    comparison = cv2.VideoCapture("testing/pysonic-test-comparison.mp4")
+    ret1, frame1 = video.read()
+    ret2, frame2 = comparison.read()
+    if not ret1 or not ret2:
+        raise Exception("Video files not found!")
+    while ret1 and ret2:
+        assert (frame1 == frame2).all()
+        ret1, frame1 = video.read()
+        ret2, frame2 = comparison.read()
+    video.release()
+    comparison.release()
+    print("Validation complete!")
